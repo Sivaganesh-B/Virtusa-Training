4c09767019a4d3d77afbe48423134224c74bda56
Sivaganesh-B <113746910+Sivaganesh-B@users.noreply.github.com>
2022-11-27 14:10:53 +0530

    Create Min_Max_Sum.cs

---
+++ Virtusa/C#/Min_Max_Sum.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+class Array_Min_Max_Sum {
+  static void Main(string []args) 
+  {
+    
+    int [] a = {1,2,3,4,5};
+    
+    int min = a.Min();
+    int max = a.Max();
+    
+    Console.WriteLine(min+max);
+    
+    
+  }
+}
